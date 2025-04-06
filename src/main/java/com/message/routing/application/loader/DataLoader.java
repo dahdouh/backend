50ad11871c6f7378d004a672e7c38e976eba09d4
Karim <karim.dahdouh@yahoo.com>
2025-04-06 18:21:44 +0200

    feat: data loader

---
+++ src/main/java/com/message/routing/application/loader/DataLoader.java
@@ -0,0 +1,48 @@
+package com.message.routing.application.loader;
+
+import com.message.routing.domain.model.*;
+import com.message.routing.output.data.service.MessageService;
+import com.message.routing.output.data.service.PartnerService;
+import com.message.routing.output.data.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+    private final Resource messageFile;
+    private final UserService userService;
+    private final PartnerService partnerService;
+    private final MessageService messageService;
+
+    private final String queueName;
+
+
+    @Autowired
+    public DataLoader(final UserService userService,
+                      final PartnerService partnerService,
+                      final MessageService messageService,
+                      @Value("${app.queue.name}") final String queueName,
+                      @Value("classpath:data/message.xml") final Resource messageFile) {
+        this.userService = userService;
+        this.partnerService = partnerService;
+        this.messageFile = messageFile;
+        this.messageService = messageService;
+        this.queueName = queueName;
+    }
+
+    @Override
+    public void run(final ApplicationArguments args) throws IOException {
+        userService.save(new User("admin", "admin"));
+        final BackOfficeMessage backOfficeMessage = new BackOfficeMessage(messageFile.getContentAsString(UTF_8));
+        messageService.save(backOfficeMessage);
+        partnerService.save(new Partner(1L, "alias1", "type1", Direction.INBOUND, "app1", FlowType.NOTIFICATION, "description1"));
+    }
+}
